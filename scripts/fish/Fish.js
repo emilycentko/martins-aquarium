388d8fb7eb15273ef46864835bb29161096a8db1
emilycentko <emily.centko@gmail.com>
2021-01-13 15:44:28 -0600

    Added fish, locations, and tips scripts  sub-directories

--- scripts/fish/Fish.js
+++ scripts/fish/Fish.js
@@ -4,7 +4,7 @@ export const Fish = (fish) => {
             <p class="fishCard__name">${fish.name}</p>
             <p class="fishCard__species">${fish.species}</p>
             <p class="fishCard__location">${fish.location}</p>
-            <p class="fishCard__length">${fish.length}</p>
+            <p class="fishCard__length">${fish.size}</p>
             <p class="fishCard__diet">${fish.food}</p>
             <img class="fishCard__img" src="${fish.image}"/>
           </div>
