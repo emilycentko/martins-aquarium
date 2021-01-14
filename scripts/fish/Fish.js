0c2425abdf40e2e29b71ec42a8816e74df3f1d6a
emilycentko <emily.centko@gmail.com>
2021-01-14 08:58:17 -0600

    Edited fish cards CSS

--- scripts/fish/Fish.js
+++ scripts/fish/Fish.js
@@ -1,12 +1,12 @@
 export const Fish = (fish) => {
     return `
     <div class="fishCard">
+            <img class="fishCard__img" src="${fish.image}"/>
             <p class="fishCard__name">${fish.name}</p>
             <p class="fishCard__species">${fish.species}</p>
             <p class="fishCard__location">${fish.location}</p>
             <p class="fishCard__length">${fish.size}</p>
             <p class="fishCard__diet">${fish.food}</p>
-            <img class="fishCard__img" src="${fish.image}"/>
           </div>
     `
 }
